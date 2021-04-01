f5d529ba95f9d0cee56ce35ad4551475f8c605c2
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2021-04-01 14:06:57 -0600

    Update __init__.py

--- src/commons/utils/__init__.py
+++ src/commons/utils/__init__.py
@@ -1,3 +1,6 @@
 """Export resources"""
 
-from .utils import store_trace_id, prepare_request_data, validate_json_schema
+from .utils import (
+    validate_json_schema, call_service, store_trace_id, prepare_request_data, build_payload, build_xml_root_causes,
+    error_xml, prettify_xml
+)
